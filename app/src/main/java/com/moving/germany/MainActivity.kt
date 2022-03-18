6f0729fa44b81f7d173f49fc6f31ade70ba7dcdb
ritwikshanker <ritwikshanker@yahoo.in>
2022-03-19 00:52:47 +0530

    Added switch and menu

--- app/src/main/java/com/moving/germany/MainActivity.kt
+++ app/src/main/java/com/moving/germany/MainActivity.kt
@@ -1,28 +1,27 @@
 package com.moving.germany
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatDelegate
 import com.moving.germany.ui.main.MainFragment
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.github.angads25.toggle.widget.LabeledSwitch
+import com.moving.germany.about.AboutFragment
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initTheme()
         setContentView(R.layout.main_activity)
-//        drawerLayout = findViewById(R.id.my_drawer_layout)
-//        actionBarDrawerToggle =
-//            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
-//        drawerLayout.addDrawerListener(actionBarDrawerToggle)
-//        actionBarDrawerToggle.syncState()
-//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
@@ -30,9 +29,82 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-//            true
-//        } else super.onOptionsItemSelected(item)
-//    }
+    private fun goToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+
+    private var darkMode = true
+
+    @Override
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_tracker, menu)
+        val switchItem = menu?.findItem(R.id.dark_mode_switch)
+        val darkSwitch = switchItem?.actionView?.findViewById<LabeledSwitch>(R.id.labeled_switch)
+        darkSwitch?.isOn = darkMode
+        darkSwitch?.setOnToggledListener { toggleableView, isOn ->
+            val isNightTheme =
+                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when (isNightTheme) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    darkMode = false
+                }
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    darkMode = true
+                }
+            }
+            nightMode(darkMode)
+            val themepref: SharedPreferences =
+                getSharedPreferences("userTheme", Context.MODE_PRIVATE)
+            val editor = themepref.edit()
+            editor.putBoolean("userTheme", darkMode)
+            editor.apply()
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about_us -> {
+                goToFragment(AboutFragment.newInstance())
+                true
+            }
+            android.R.id.home -> {
+                super.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initTheme() {
+        val themepref: SharedPreferences = getSharedPreferences("userTheme", Context.MODE_PRIVATE)
+        val editor = themepref.edit()
+
+        if (themepref.contains("userTheme")) {
+            darkMode = themepref.getBoolean("userTheme", false)
+        } else {
+            val isNightTheme =
+                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            darkMode = isNightTheme == Configuration.UI_MODE_NIGHT_YES
+            editor.putBoolean("userTheme", !true)
+            editor.apply()
+        }
+        nightMode(darkMode)
+
+    }
+
+    private fun nightMode(on: Boolean) {
+        if (on)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+    }
+
 }
